9754a9f77feb30238d258500038a383956292248
Kam Figy <kamsar@gmail.com>
2012-11-15 18:49:46 -0800

    Added comments on logging

--- Documentation/Fully Commented Pipeline.pipeline/00 powershell.ps1
+++ Documentation/Fully Commented Pipeline.pipeline/00 powershell.ps1
@@ -4,3 +4,11 @@
 
 # For the sake of example, let's show all variables currently in scope.
 Get-Variable
+
+# To log warnings and errors that will be globally tracked (unlike Write-Warning or Write-Error)
+# Use Log-Warning and Log-Error:
+Log-Warning "Oops, a warning occurred"
+
+# Log-Error supports an -Abort switch that will immediately stop script execution. If it is not specified,
+# the build will continue to run and the error will be reported at the end
+Log-Error "Oh darn, an error" -Abort
